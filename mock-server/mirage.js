dfa0741cd06aa042fe5170748197e9260e279e2f
Frank Liang <liangfeng610@gmail.com>
2020-12-08 20:26:49 +1300

    import all json files and config mirage, got error

--- mock-server/mirage.js
+++ mock-server/mirage.js
@@ -1,6 +1,10 @@
-import { createServer, Model, Response } from 'miragejs';
-import studentsData from './students.json';
-import usersData from './users.json';
+import { belongsTo, createServer, hasMany, Model, Response } from 'miragejs';
+import studentsData from './student.json';
+import usersData from './user.json';
+import courseTypesData from './course_type.json';
+import coursesData from './course.json';
+import studentTypesData from './student_type.json';
+import studentCoursesData from './student_course.json';
 
 export function makeServer({ environment = 'development' } = {}) {
   let server = createServer({
@@ -8,18 +12,30 @@ export function makeServer({ environment = 'development' } = {}) {
 
     models: {
       user: Model,
-      student: Model,
+      studentType: Model,
+      student: Model.extend({
+        studentCourses: hasMany(),
+        type: belongsTo('studentType'),
+      }),
+      courseType: Model,
+      course: Model,
+      studentCourse: Model.extend({
+        student: belongsTo(),
+      }),
     },
 
     seeds(server) {
       usersData.forEach((user) => server.create('user', user));
+      studentCoursesData.forEach((course) => server.create('studentCourse', course));
       studentsData.forEach((student) => server.create('student', student));
+      studentTypesData.forEach((type) => server.create('studentType', type));
+      coursesData.forEach((course) => server.create('course', course));
+      courseTypesData.forEach((type) => server.create('courseType', type));
     },
 
     routes() {
       this.passthrough((request) => {
-        if (request.url === '/_next/static/development/_devPagesManifest.json')
-          return true;
+        if (request.url === '/_next/static/development/_devPagesManifest.json') return true;
       });
 
       this.namespace = '/api';
@@ -80,14 +96,14 @@ export function makeServer({ environment = 'development' } = {}) {
       //? Students List: Get all students
       this.get('/students', (schema, request) => {
         const { db } = schema;
+        console.log('schema :>> ', schema);
+        console.log('db :>> ', db);
         const { queryParams } = request;
         const studentsDataFromDb = db.students;
 
         //? Search Query
         const query = queryParams.query || '';
-        const queriedData = studentsDataFromDb.filter((student) =>
-          student.name.includes(query)
-        );
+        const queriedData = studentsDataFromDb.filter((student) => student.name.includes(query));
 
         //? Pagination
         const page = parseInt(queryParams.page, 10) || 1;
